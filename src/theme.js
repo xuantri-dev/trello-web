c93eee3f0019c53ab558bc956c03b3a9928a31ef
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-06-18 17:45:29 +0700

    dark mode và light mode trong mui

--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,35 @@
-import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+import { cyan, orange, teal, deepOrange } from "@mui/material/colors";
 
-// Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    mode: "light", // default is light
-    primary: {
-      main: "#556cd6",
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
+        text: {
+          primary: "#000",
+          secondary: "#666",
+        },
+        background: {
+          default: "#fff",
+          paper: "#f5f5f5",
+        },
+      },
     },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
-    },
-    text: {
-      secondary: red[500],
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange,
+        text: {
+          primary: "#fff",
+          secondary: "#ccc",
+        },
+        background: {
+          default: "#121212",
+          paper: "#1e1e1e",
+        },
+      },
     },
   },
 });
