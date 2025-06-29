264188f89a9c555cb866e8dd911452883b82dd65
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-06-29 22:47:55 +0700

    thanh board bar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,17 +1,161 @@
 import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import VpnLockIcon from "@mui/icons-material/VpnLock";
+import AddToDriveIcon from "@mui/icons-material/AddToDrive";
+import BoltIcon from "@mui/icons-material/Bolt";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import Avatar from "@mui/material/Avatar";
+import AvatarGroup from "@mui/material/AvatarGroup";
+import { Tooltip } from "@mui/material";
+import Button from "@mui/material/Button";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+
+const MENU_STYLES = {
+  color: "primary.main",
+  backgroundColor: "white",
+  border: "none",
+  paddingX: "5px",
+  borderRadius: "4px",
+  "& .MuiSvgIcon-root": {
+    color: "primary.main",
+  },
+  "&:hover": {
+    bgcolor: "primary.50",
+  },
+};
 
 function BoardBar() {
   return (
     <Box
       sx={{
-        backgroundColor: "primary.dark",
         width: "100%",
         height: (theme) => theme.trello.boardBarHeight,
         display: "flex",
         alignItems: "center",
+        justifyContent: "space-between",
+        gap: 2,
+        paddingX: 2,
+        overflowX: "auto",
+        borderTop: "1px solid #00bfa5",
       }}
     >
-      Board Bar
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label="XuanTriDev MERN Stack Board"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label="Filters"
+          clickable
+        />
+      </Box>
+
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}>
+          Invite
+        </Button>
+
+        <AvatarGroup
+          max={7}
+          sx={{ "& .MuiAvatar-root": { width: 34, height: 34, fontSize: 16 } }}
+        >
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://images.icon-icons.com/2643/PNG/512/male_man_people_person_avatar_white_tone_icon_159363.png"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?semt=ais_items_boosted&w=740"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4841.jpg?semt=ais_items_boosted&w=740"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://img.freepik.com/premium-vector/cute-woman-avatar-profile-vector-illustration_1058532-14592.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://cdn.vectorstock.com/i/1000v/06/18/male-avatar-profile-picture-vector-10210618.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://t4.ftcdn.net/jpg/11/66/06/77/360_F_1166067709_2SooAuPWXp20XkGev7oOT7nuK1VThCsN.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://images.icon-icons.com/2643/PNG/512/male_man_people_person_avatar_white_tone_icon_159363.png"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?semt=ais_items_boosted&w=740"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4841.jpg?semt=ais_items_boosted&w=740"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://img.freepik.com/premium-vector/cute-woman-avatar-profile-vector-illustration_1058532-14592.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://cdn.vectorstock.com/i/1000v/06/18/male-avatar-profile-picture-vector-10210618.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="xuantridev">
+            <Avatar
+              alt="xuantridev"
+              src="https://t4.ftcdn.net/jpg/11/66/06/77/360_F_1166067709_2SooAuPWXp20XkGev7oOT7nuK1VThCsN.jpg"
+            />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   );
 }
