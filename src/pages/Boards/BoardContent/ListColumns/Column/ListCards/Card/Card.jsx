55a9018f6fedd6fe664cfc0a4f9d27ec101d0c89
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-08-19 18:21:25 +0700

    fix_bug_flickering_37.2

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -44,6 +44,9 @@ function Card({ card }) {
         cursor: "pointer",
         boxShadow: "0 1px 1px rgba(0, 0, 0, 0.2)",
         overflow: "unset",
+        display: card?.FE_PlaceholderCard ? "none" : "block",
+        // overflow: card?.FE_PlaceholderCard ? "hidden" : "unset",
+        // display: card?.FE_PlaceholderCard ? "0px" : "unset",
       }}
     >
       {card?.cover && <CardMedia sx={{ height: 140 }} image={card?.cover} />}
