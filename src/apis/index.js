283ea56e991332dcf9b939434e181ed30c8550d0
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-09-15 06:43:49 +0700

    axios call api

---
+++ src/apis/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { API_ROOT } from "~/utils/constants";
+
+/*  
+ Lưu ý: đối với việc sử dụng axios, tất cả function ở dưới sẽ chỉ request và lấy data từ response luôn, không có try catch hay then catch gì để bắt lỗi
+ Lí do là vì ở phía FE không nhất thiết phải làm như vậy đối với mọi request với nó sẽ dư thừa code catch lỗi quá nhiều
+ Giải pháp clean code gọn gàng đó là sẽ catch lỗi tập trung tại một nơi bằng cách tận dụnng một thứ cực kì mạnh mẽ trong axios đó là Interceptors
+ Interceptors là cách mà sẽ đánh chặn giữa request hoặc response để xử lí logic theo mong muốn
+*/
+export const fetchBoardDetailsAPI = async (boardId) => {
+  const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`);
+  // axios sẽ trả kết quả về qua property của nó là data
+  return response.data;
+};
