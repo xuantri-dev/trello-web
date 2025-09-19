9b8ca87032d60af133e0516d643e586399f13b17
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-09-19 07:31:19 +0700

    fix bug drag drop

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -4,8 +4,8 @@ import { mapOrder } from "~/utils/sorts";
 import {
   DndContext,
   // PointerSensor,
-  MouseSensor,
-  TouchSensor,
+  // MouseSensor,
+  // TouchSensor,
   useSensor,
   useSensors,
   DragOverlay,
@@ -16,6 +16,8 @@ import {
   // rectIntersection,
   getFirstCollision,
 } from "@dnd-kit/core";
+import { MouseSensor, TouchSensor } from "~/customLibraries/DndKitSensors";
+
 import { arrayMove } from "@dnd-kit/sortable";
 import { useEffect, useState, useCallback, useRef } from "react";
 import { cloneDeep, isEmpty } from "lodash";
