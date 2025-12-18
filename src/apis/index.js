5b5cd2333c98a595c7990ae2ac9ef21c426bbebc
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-12-18 18:24:46 +0700

    buoi 5

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import authorizeAxiosInstance from '~/utils/authorizeAxios'
 import { API_ROOT } from '~/utils/constants'
 
 /*
@@ -17,7 +17,7 @@ import { API_ROOT } from '~/utils/constants'
 // };
 
 export const updateBoardDetailsAPI = async (boardId, updateData) => {
-  const response = await axios.put(
+  const response = await authorizeAxiosInstance.put(
     `${API_ROOT}/v1/boards/${boardId}`,
     updateData
   )
@@ -25,7 +25,7 @@ export const updateBoardDetailsAPI = async (boardId, updateData) => {
 }
 
 export const moveCardToDifferentColumnAPI = async (updateData) => {
-  const response = await axios.put(
+  const response = await authorizeAxiosInstance.put(
     `${API_ROOT}/v1/boards/support/moving_card`,
     updateData
   )
@@ -34,12 +34,12 @@ export const moveCardToDifferentColumnAPI = async (updateData) => {
 
 /* Column */
 export const createNewColumnAPI = async (newColumnData) => {
-  const response = await axios.post(`${API_ROOT}/v1/columns`, newColumnData)
+  const response = await authorizeAxiosInstance.post(`${API_ROOT}/v1/columns`, newColumnData)
   return response.data
 }
 
 export const updateColumnDetailsAPI = async (columnId, updateData) => {
-  const response = await axios.put(
+  const response = await authorizeAxiosInstance.put(
     `${API_ROOT}/v1/columns/${columnId}`,
     updateData
   )
@@ -47,12 +47,12 @@ export const updateColumnDetailsAPI = async (columnId, updateData) => {
 }
 
 export const deleteColumnDetailsAPI = async (columnId) => {
-  const response = await axios.delete(`${API_ROOT}/v1/columns/${columnId}`)
+  const response = await authorizeAxiosInstance.delete(`${API_ROOT}/v1/columns/${columnId}`)
   return response.data
 }
 
 /* Card */
 export const createNewCardAPI = async (newCardData) => {
-  const response = await axios.post(`${API_ROOT}/v1/cards`, newCardData)
+  const response = await authorizeAxiosInstance.post(`${API_ROOT}/v1/cards`, newCardData)
   return response.data
 }
--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -341,6 +341,7 @@ function Column({ column }) {
               />
               <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
                 <Button
+                  className="interceptor-loading"
                   onClick={addNewCard}
                   variant="contained"
                   color="success"
