fd72bc653dd582d2f87c4cfe31482fdb1435be43
TuiTenXT <tritxps28458@fpt.edu.vn>
2025-12-31 18:09:46 +0700

    xyz

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
 
 import Board from '~/pages/Boards/_id'
 import NotFound from '~/pages/404/NotFound'
 import Auth from '~/pages/Auth/Auth'
 import AccountVerification from '~/pages/Auth/AccountVerification'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from '~/redux/user/userSlice'
+
+/**
+ * Giải pháp Clean Code trong việc xác định các route nào cần đăng nhập tài khoản xong thì mới cho truy cập
+ * Sử dụng <Outlet /> của react-router-dom để hiển thị các Child Route (Xem cách sử dụng trong App() bên dưới)
+ * https://reactrouter.com/en/main/components/outlet
+ * https://www.robinwieruch.de/react-router-private-routes/
+ */
+const ProtectedRoute = ({ user }) => {
+  // console.log(user)
+  if (!user) return <Navigate to='/login' replace={true} />
+  return <Outlet />
+}
 
 function App() {
+  const currentUser = useSelector(selectCurrentUser)
+
   return (
     <Routes>
       {/* Redirect Route */}
@@ -15,8 +31,13 @@ function App() {
         <Navigate to="/boards/68dd328053a2c1b1e7949ca6" replace={true} />
       } />
 
-      {/* Board Details */}
-      <Route path='/boards/:boardId' element={<Board />} />
+      {/* Protected Routes (hiểu đơn giản trong dự án của chúng ta là những route chỉ cho truy cập sau khi đã login) */}
+      <Route element={<ProtectedRoute user={currentUser} />} >
+        {/* <Outlet /> của react-router-dom sẽ chạy vào các child route trong này */}
+
+        {/* Board Details */}
+        <Route path='/boards/:boardId' element={<Board />} />
+      </Route>
 
       {/* Authentication */}
       <Route path='/login' element={<Auth />} />
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,37 @@ import { configureStore } from '@reduxjs/toolkit'
 import { activeBoardReducer } from './activeBoard/activeBoardSlice'
 import { userReducer } from './user/userSlice'
 
-export const store = configureStore({
-  reducer: {
-    activeBoard: activeBoardReducer,
-    user: userReducer
-  }
+
+/**
+ * Cấu hình redux-persist
+ * https://www.npmjs.com/package/redux-persist
+ * Tìm hiếm how to use redux persist with redux toolkit để dễ hiểu hơn
+ */
+import { combineReducers } from '@reduxjs/toolkit' // lưu ý đã có sẵn redux trong node_modules bởi vì khi cài @reduxjs/toolkit là đã có luôn
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // default là localstorage
+
+// Cấu hình persist
+export const rootPersistConfig = {
+  key: 'root', // key của cái persist do chúng ta chỉ định, cứ để mặc định là root
+  storage: storage, // biến storage ở trên - lưu vào localstorage
+  whitelist: ['user'] // định nghĩa các slice dữ liệu ĐƯỢC PHÉP duy trì qua mỗi lần f5 trình duyệt
+  // blacklist: ['user'] // định nghĩa các slice dữ liệu KHÔNG ĐƯỢC PHÉP duy trì qua mỗi lần f5 trình duyệt
+}
+
+// Combine các reducers trong dự án ở đây
+const reducers = combineReducers({
+  activeBoard: activeBoardReducer,
+  user: userReducer
 })
+
+// Thực hiện persist Reducer
+const persistedReducers = persistReducer(rootPersistConfig, reducers)
+
+export const store = configureStore({
+  reducer: persistedReducers,
+  // Fix warning error when implement redux-persist
+  // https://stackoverflow.com/questions/61704805/getting-an-error-a-non-serializable-value-was-detected-in-the-state-when-using
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
+})
+
