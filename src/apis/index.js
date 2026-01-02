2cd09f18d4bcf097dbaa1652b9a7cf5ee99ede7d
TuiTenXT <tritxps28458@fpt.edu.vn>
2026-01-02 16:40:38 +0700

    xyz

--- src/apis/index.js
+++ src/apis/index.js
@@ -70,3 +70,8 @@ export const verifyUserAPI = async (data) => {
   toast.success('Account verified successfully! Now you can login to enjoy our services! Have a good day!', { theme: 'colored' })
   return response.data
 }
+
+export const refreshTokenAPI = async () => {
+  const response = await authorizeAxiosInstance.get(`${API_ROOT}/v1/users/refresh_token`)
+  return response.data
+}
