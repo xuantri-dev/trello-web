325ed482aca2fa0119dc7faec1202bf828c73f1f
TuiTenXT <tritxps28458@fpt.edu.vn>
2026-01-14 16:32:55 +0700

    xyz

--- src/apis/index.js
+++ src/apis/index.js
@@ -75,3 +75,8 @@ export const refreshTokenAPI = async () => {
   const response = await authorizeAxiosInstance.get(`${API_ROOT}/v1/users/refresh_token`)
   return response.data
 }
+
+export const fetchBoardsAPI = async (searchPath) => {
+  const response = await authorizeAxiosInstance.get(`${API_ROOT}/v1/boards${searchPath}`)
+  return response.data
+}
