51d5e61c224d37b141297dd9030979cfdc4f2e7b
TuiTenXT <tritxps28458@fpt.edu.vn>
2026-02-04 22:17:31 +0700

    xyz

--- src/main.jsx
+++ src/main.jsx
@@ -29,10 +29,16 @@ const persistor = persistStore(store)
 import { injectStore } from '~/utils/authorizeAxios'
 injectStore(store)
 
+// Cấu hình Socket-io phía client tại đây và export ra biến socketIoInstance
+// https://socket.io/how-to/use-with-react
+import { io } from 'socket.io-client'
+import { API_ROOT } from './utils/constants'
+export const socketIoInstance = io(API_ROOT)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/'>
-    <Provider store={store}>
-      <PersistGate persistor={persistor} >
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter basename='/'>
         <CssVarsProvider theme={theme}>
           <ConfirmProvider
             defaultOptions={{
@@ -49,7 +55,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <ToastContainer position="bottom-left" theme="colored" />
           </ConfirmProvider>
         </CssVarsProvider>
-      </PersistGate>
-    </Provider>
-  </BrowserRouter>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Cho phép thằng Vite sử dụng được process.env, mặc định thì không mà sẽ phải dùng import.meta.env
+  // https://github.com/vitejs/vite/issues/1973
+  define: {
+    'process.env': process.env
+  },
   plugins: [react(), svgr()],
   // base: './'
   resolve: {
-    alias: [{ find: "~", replacement: "/src" }],
-  },
-});
+    alias: [{ find: '~', replacement: '/src' }]
+  }
+})
